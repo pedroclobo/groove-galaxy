481be9ecf86c06dfae823c14326b30db533db751
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-11-24 11:03:05 +0000

    feat: add help menu

--- src/pt/tecnico/SecureGroove.java
+++ src/pt/tecnico/SecureGroove.java
@@ -2,6 +2,45 @@ package pt.tecnico;
 
 public class SecureGroove {
     public static void main(String[] args) {
-        System.out.println("Hello SecureGroove!");
+        if (args.length == 0) {
+            printHelp();
+            return;
+        }
+
+        String commandKind = args[0];
+        switch (commandKind) {
+            case "protect":
+                throw new UnsupportedOperationException("Not implemented yet");
+
+            case "check":
+                throw new UnsupportedOperationException("Not implemented yet");
+
+            case "unprotect":
+                throw new UnsupportedOperationException("Not implemented yet");
+
+            default:
+                printHelp();
+                break;
+        }
+    }
+
+    private static void printHelp() {
+        System.err.println("Argument(s) missing!");
+        System.err.println("Usage: secure-groove help");
+        System.err.println("Usage: secure-groove protect (input-file) (output-file) (key-file)");
+        System.err.println("Usage: secure-groove check (input-file) (key-file)");
+        System.err.println("Usage: secure-groove unprotect (input-file) (output-file) (key-file)");
+    }
+
+    private static void protect(String input, String output, String key) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private static void check(String input, String key) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    private static void unprotect(String input, String output, String key) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
